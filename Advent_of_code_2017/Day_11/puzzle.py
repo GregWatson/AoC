b51b94314e05b56072d8189756aab4e4571bb6ac
GregWatson <gwatson@gmail.com>
2025-04-25 10:43:35 -0700

    AoC 2017 Day 11 done.

--- Advent_of_code_2017/Day_11/puzzle.py
+++ Advent_of_code_2017/Day_11/puzzle.py
@@ -29,23 +29,45 @@ def load_db():
         lines = f.readlines()
     return [ l.strip() for l in lines ]
 
+dirs = ['s', 'n', 'ne', 'nw', 'se', 'sw']
 
-
-def doPart1(db):
+def distance(x, y):
+    # for every 2 x I get a free y
+    xa = abs(x); ya = abs(y)
+    print(f"distance: xa = {xa} ya = {ya}")
+    if ya > xa//2: return int(xa + (ya - xa//2))
+    return int(xa)
+  
+def doPart12(db):
     count = 0
-    return count
-
-def doPart2(db):
-    count = 0
-    return count
+    l = db[0].split(',')
+    x = 0; y = 0
+    mx = 0
+    for d in l: 
+        if d == 'n':
+            y -= 1
+        elif d == 's':
+            y += 1
+        elif d == 'ne':
+            x += 1; y -= 0.5
+        elif d == 'sw':
+            x -= 1; y += 0.5
+        elif d == 'nw':
+            x -= 1; y -= 0.5
+        elif d == 'se':
+            x += 1; y += 0.5
+        else:
+            print(f"Unknown direction {d}")
+        di = distance(x,y)
+        if di > mx:
+            mx = di
+        #print(f"dir = {d} x = {x} y = {y} di = {di}")
+    print(f"part 1: {di}")
+    print(f"part 2: {mx}")
 
 #---------------------------------------------------------------------------------------
 # Load input
 db = load_db()
 
 #sys.setrecursionlimit(10000)
-p1 = doPart1(db)
-print(f"Part 1 is {p1}\n\n")
-
-p2 = doPart2(db)
-print(f"\n\n\n\nPart 2 is {p2}")
+p1 = doPart12(db)
