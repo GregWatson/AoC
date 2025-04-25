2ceeb0f4d40331d629ae0750b31342f993e1e5f8
GregWatson <gwatson@gmail.com>
2025-04-25 01:13:26 -0700

    AoC 2017 Day 11 created not started.

---
+++ Advent_of_code_2017/Day_11/puzzle.py
@@ -0,0 +1,51 @@
+#import argparse
+import sys
+import re
+import functools
+import array
+import copy
+import hashlib
+import string
+import itertools
+import math
+import time
+
+BLENDER=False
+
+# --- Blender ---
+if BLENDER:
+    import bpy
+    sys.path.append(r'C:\cygwin64\home\gwatson\AOC')
+    from blenderLib import *
+# ---------------
+
+input_file_name = 'input.txt'
+print_on = False
+BIGNUM=10000000
+MAX=141; MINSAVED=100; NUMCHEATS=20
+
+def load_db():
+    with open(input_file_name) as f:
+        lines = f.readlines()
+    return [ l.strip() for l in lines ]
+
+
+
+def doPart1(db):
+    count = 0
+    return count
+
+def doPart2(db):
+    count = 0
+    return count
+
+#---------------------------------------------------------------------------------------
+# Load input
+db = load_db()
+
+#sys.setrecursionlimit(10000)
+p1 = doPart1(db)
+print(f"Part 1 is {p1}\n\n")
+
+p2 = doPart2(db)
+print(f"\n\n\n\nPart 2 is {p2}")
